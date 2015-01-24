f697267e2a5ee75f425e12fa47ac9b44a9d30f78
Adam Gough <a@adamgough.ca>
2015-01-24 14:59:43 -0800

    Cleaned up GUI code

--- src/GUI.java
+++ src/GUI.java
@@ -14,139 +14,166 @@ public class GUI implements ActionListener {
     }
     
     public void initGUI(String title) {
-        JFrame frame = new JFrame(title);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        panel.getInputMap().put(KeyStroke.getKeyStroke("F2"), "new");
-        //panel.getActionMap().put(this, "new");
 
-        JEditorPane filePane = new JEditorPane();
-        filePane.setBorder(new TitledBorder("Files"));
-        filePane.setPreferredSize(new Dimension(200, 0));
-
-        JEditorPane editorPane = new JEditorPane(); // multiple files?
-        JEditorPane consolePane = new JEditorPane();
-        JEditorPane errorPane = new JEditorPane();
-
-        JScrollPane fileScrollPane = new JScrollPane(filePane);
-        JScrollPane editorScrollPane = new JScrollPane(editorPane);
-        JScrollPane consoleScrollPane = new JScrollPane(consolePane);
-        JScrollPane errorScrollPane = new JScrollPane(errorPane);
-
-        JTabbedPane tabbedPane = new JTabbedPane();
-        tabbedPane.add("Console", consoleScrollPane);
-        tabbedPane.add("Errors", errorScrollPane);
-        tabbedPane.setPreferredSize(new Dimension(0, 200));
-
-        JSplitPane splitPane2 = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-        splitPane2.add(editorScrollPane);
-        splitPane2.add(tabbedPane);
-        splitPane2.setOneTouchExpandable(true);
-
-        JSplitPane splitPane1 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        splitPane1.add(fileScrollPane);
-        splitPane1.add(splitPane2);
-        splitPane1.setOneTouchExpandable(true);
+        // ---------- //
 
         JMenuBar menuBar = new JMenuBar();
 
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem newMenuItem = new JMenuItem("New");
-        newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-        newMenuItem.setActionCommand("new");
-        newMenuItem.addActionListener(this);
-        JMenuItem openMenuItem = new JMenuItem("Open");
-        openMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
-        JMenuItem saveMenuItem = new JMenuItem("Save");
-        saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
-        JMenuItem saveAsMenuItem = new JMenuItem("Save As");
-        saveAsMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK + ActionEvent.SHIFT_MASK));
-        JMenuItem printMenuItem = new JMenuItem("Print");
-        printMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
-        JMenuItem quitMenuItem = new JMenuItem("Quit");
-        quitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
-        fileMenu.add(newMenuItem);
-        fileMenu.add(openMenuItem);
-        fileMenu.add(saveMenuItem);
-        fileMenu.add(saveAsMenuItem);
-        fileMenu.add(printMenuItem);
-        fileMenu.add(quitMenuItem);
+            JMenu fileMenu = new JMenu("File");
 
-        JMenu editMenu = new JMenu("Edit");
-        JMenuItem cutMenuItem = new JMenuItem("Cut");
-        cutMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
-        JMenuItem copyMenuItem = new JMenuItem("Copy");
-        copyMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
-        JMenuItem pasteMenuItem = new JMenuItem("Paste");
-        pasteMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
-        JMenuItem selectAllMenuItem = new JMenuItem("Select All");
-        selectAllMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
-        editMenu.add(cutMenuItem);
-        editMenu.add(copyMenuItem);
-        editMenu.add(pasteMenuItem);
-        editMenu.add(selectAllMenuItem);
+                JMenuItem newMenuItem = new JMenuItem("New");
+                newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
 
-        JMenu helpMenu = new JMenu("Help");
-        JMenuItem viewHelpMenuItem = new JMenuItem("View Help");
-        viewHelpMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.CTRL_MASK));
-        JMenuItem aboutMenuItem = new JMenuItem("About my IDE");
-        aboutMenuItem.setActionCommand("help");
-        aboutMenuItem.addActionListener(this);
-        aboutMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK));
-        helpMenu.add(viewHelpMenuItem);
-        helpMenu.add(aboutMenuItem);
+                JMenuItem openMenuItem = new JMenuItem("Open");
+                openMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
 
-        JToolBar toolBar = new JToolBar("Tool Bar");
+                JMenuItem saveMenuItem = new JMenuItem("Save");
+                saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
 
-        JButton newToolBarButton = new JButton();
-        newToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/page_add.png"));
+                JMenuItem saveAsMenuItem = new JMenuItem("Save As");
+                saveAsMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK + ActionEvent.SHIFT_MASK));
 
-        JButton saveToolBarButton = new JButton();
-        saveToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/page_save.png"));
+                JMenuItem printMenuItem = new JMenuItem("Print");
+                printMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
 
-        JButton cutToolBarButton = new JButton();
-        cutToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/cut.png"));
+                JMenuItem quitMenuItem = new JMenuItem("Quit");
+                quitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
 
-        JButton copyToolBarButton = new JButton();
-        copyToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/page_copy.png"));
+            fileMenu.add(newMenuItem);
+            fileMenu.add(openMenuItem);
+            fileMenu.add(saveMenuItem);
+            fileMenu.add(saveAsMenuItem);
+            fileMenu.add(new JSeparator());
+            fileMenu.add(printMenuItem);
+            fileMenu.add(new JSeparator());
+            fileMenu.add(quitMenuItem);
 
-        JButton pasteToolBarButton = new JButton();
-        pasteToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/page_paste.png"));
+            JMenu editMenu = new JMenu("Edit");
 
-        JButton printToolBarButton = new JButton();
-        printToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/printer.png"));
+                JMenuItem cutMenuItem = new JMenuItem("Cut");
+                cutMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
 
-        JButton runToolBarButton = new JButton();
-        runToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/resultset_next.png"));
+                JMenuItem copyMenuItem = new JMenuItem("Copy");
+                copyMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
 
-        JButton helpToolBarButton = new JButton();
-        helpToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/help.png"));
+                JMenuItem pasteMenuItem = new JMenuItem("Paste");
+                pasteMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
 
-        JButton closeToolBarButton = new JButton();
-        closeToolBarButton.setIcon(new ImageIcon("/home/gougha/workspace/idiot-ide/src/img/cancel.png"));
+                JMenuItem selectAllMenuItem = new JMenuItem("Select All");
+                selectAllMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
 
-        toolBar.add(newToolBarButton);
-        toolBar.add(saveToolBarButton);
-        toolBar.add(cutToolBarButton);
-        toolBar.add(copyToolBarButton);
-        toolBar.add(pasteToolBarButton);
-        toolBar.add(printToolBarButton);
-        toolBar.add(runToolBarButton);
-        toolBar.add(helpToolBarButton);
-        toolBar.add(closeToolBarButton);
+            editMenu.add(cutMenuItem);
+            editMenu.add(copyMenuItem);
+            editMenu.add(pasteMenuItem);
+            editMenu.add(new JSeparator());
+            editMenu.add(selectAllMenuItem);
+
+            JMenu helpMenu = new JMenu("Help");
+
+                JMenuItem viewHelpMenuItem = new JMenuItem("View Help");
+                viewHelpMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.CTRL_MASK));
+
+                JMenuItem aboutMenuItem = new JMenuItem("About My IDE");
+                aboutMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK));
+
+            helpMenu.add(viewHelpMenuItem);
+            helpMenu.add(aboutMenuItem);
 
         menuBar.add(fileMenu);
         menuBar.add(editMenu);
         menuBar.add(helpMenu);
 
-        panel.add(splitPane1);
-        panel.setSize(new Dimension(800, 600));
+        // ---------- //
+
+        JToolBar toolBar = new JToolBar();
+
+            JButton newToolBarButton = new JButton("New");
+
+            JButton openToolBarButton = new JButton("Open");
+
+            JButton saveToolBarButton = new JButton("Save");
+
+            JButton saveAsToolBarButton = new JButton("Save As");
+
+            JButton closeToolBarButton = new JButton("Close");
+
+            JButton printToolBarButton = new JButton("Print");
+
+            JButton cutToolBarButton = new JButton("Cut");
+
+            JButton copyToolBarButton = new JButton("Copy");
+
+            JButton pasteToolBarButton = new JButton("Paste");
+
+            JButton runToolBarButton = new JButton("Run");
+
+        toolBar.add(newToolBarButton);
+        toolBar.add(openToolBarButton);
+        toolBar.add(saveToolBarButton);
+        toolBar.add(saveAsToolBarButton);
+        toolBar.add(closeToolBarButton);
+        toolBar.add(printToolBarButton);
+        toolBar.add(cutToolBarButton);
+        toolBar.add(copyToolBarButton);
+        toolBar.add(pasteToolBarButton);
+        toolBar.add(runToolBarButton);
+
+        // ---------- //
+
+        JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+
+            // leftSplitPane currently contains the file browser
+            JSplitPane leftSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+
+                JEditorPane filePane = new JEditorPane();
+                JScrollPane fileScrollPane = new JScrollPane(filePane);
+
+            leftSplitPane.add(fileScrollPane);
+
+            // rightSplitPane currently contains two tabbed panes for editing files and output
+            JSplitPane rightSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+
+                JTabbedPane topTabbedPane = new JTabbedPane();
+
+                    // TODO: real tabbed file editor
+
+                    // JEditorPane(s) used as a placeholder, could be changed in future
+                    JEditorPane file1 = new JEditorPane();
+                    JScrollPane file1ScrollPane = new JScrollPane(file1);
+
+                    JEditorPane file2 = new JEditorPane();
+                    JScrollPane file2ScrollPane = new JScrollPane(file2);
+
+                topTabbedPane.add(file1ScrollPane, "file1.idiot");
+                topTabbedPane.add(file2ScrollPane, "file2.idiot");
+
+                JTabbedPane bottomTabbedPane = new JTabbedPane();
+
+                    // JEditorPane(s) used as a placeholder, could be changed in future
+                    JEditorPane consolePane = new JEditorPane();
+                    JEditorPane errorPane = new JEditorPane();
+
+                bottomTabbedPane.add(consolePane, "Console");
+                bottomTabbedPane.add(errorPane, "Errors");
+
+            rightSplitPane.add(topTabbedPane);
+            rightSplitPane.add(bottomTabbedPane);
+
+        mainSplitPane.add(leftSplitPane);
+        mainSplitPane.add(rightSplitPane);
+
+        // ---------- //
+
+        panel.add(mainSplitPane);
+
         frame.add(panel);
 
         frame.setJMenuBar(menuBar);
         frame.add(toolBar, BorderLayout.NORTH);
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension(800, 600));
         frame.pack();
         frame.setLocationRelativeTo(null);
