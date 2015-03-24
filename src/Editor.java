70a03fcea97f9b664ff235c24f4157d70151a9c3
knorum0 <knorum0@localhost>
2015-03-24 13:52:40 -0700

    Fixed scrollbar in edit windows

--- src/Editor.java
+++ src/Editor.java
@@ -3,29 +3,32 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class Editor extends JTabbedPane {
-    private ArrayList<JEditorPane> tabList = new ArrayList<JEditorPane>();
+    private ArrayList<JScrollPane> tabList = new ArrayList<JScrollPane>();
+    private ArrayList<JEditorPane> editList = new ArrayList<JEditorPane>();
 
     public Editor() {
         super();
     }
 
     public void newTab(String title) {
-        this.tabList.add(new JEditorPane());
+    	this.editList.add(new JEditorPane());
+        this.tabList.add(new JScrollPane(this.editList.get(this.editList.size()-1)));
         this.add(title, tabList.get(tabList.size() - 1));
+        this.setSelectedIndex(this.tabList.size() - 1);
     }
     
     public JEditorPane getTab(int index) {
-        return this.tabList.get(index);
+        return this.editList.get(index);
     }
     
     public JEditorPane getLastTab() {
-        return this.tabList.get(this.tabList.size() - 1);
+        return this.editList.get(this.tabList.size() - 1);
     }
     
     public String getActiveTabTitle(){
 		return this.getTitleAt(this.getSelectedIndex());
     }
     public JEditorPane getActiveTab(){
-		return this.tabList.get(this.getSelectedIndex());
+		return this.editList.get(this.getSelectedIndex());
     }
 }
